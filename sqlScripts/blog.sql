a20721f3de171ff97cee709c776a74d316d92bdf
ChadH28 <chadhoosain@gmail.com>
2021-04-23 16:22:22 +0200

    Made a blog table

---
+++ sqlScripts/blog.sql
@@ -0,0 +1,9 @@
+CREATE TABLE blog (
+    title VARCHAR(20),
+    blog_image data BLOB,
+    blog_content VARCHAR(35),
+    blog_id VARCHAR(15),
+    topic VARCHAR(35),
+    datestamp DATE,
+    timestamp time,
+);
